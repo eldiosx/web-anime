2fbc26675fd45fc85fdf9694c84be2e1177eff3e
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-01-24 14:19:16 +0100

    Header v1.0

--- js/smart.js
+++ js/smart.js
@@ -3,7 +3,7 @@ window.onload = function(){
 }
 
 function mainmenu() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("main-nav");
   if (x.className === "main-nav") {
     x.className += " responsive";
   } else {
