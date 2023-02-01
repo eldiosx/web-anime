b72408652393d4313197ed5b2e826b8f1fbd03c2
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-02-01 21:27:56 +0100

    MainBodyChanger Update

--- js/smart.js
+++ js/smart.js
@@ -12,29 +12,6 @@ function mainmenu() {
   }
 }
 
-//Sub-Filter
-
-function w3AddClass(element, name) {
-  var i, a1, a2;
-  a1 = element.className.split(" ");
-  a2 = name.split(" ");
-  for (i = 0; i < a2.length; i++) {
-    if (a1.indexOf(a2[i]) == -1) { element.className += " " + a2[i]; }
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, a1, a2;
-  a1 = element.className.split(" ");
-  a2 = name.split(" ");
-  for (i = 0; i < a2.length; i++) {
-    while (a1.indexOf(a2[i]) > -1) {
-      a1.splice(a1.indexOf(a2[i]), 1);
-    }
-  }
-  element.className = a1.join(" ");
-}
-
 //Theme
 
 function themeToggle() {
@@ -50,3 +27,62 @@ function themeToggle() {
 
   document.getElementById("theme").textContent = localStorage.getItem("theme");
 }
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+//Smart-Animes (experimental)
+function aot() {
+  $("#include-body").load("/html/animes/AttackOnTitan.html");
+};
+function dn() {
+  $("#include-body").load("/html/animes/DeathNote.html");
+};
+function dcb() {
+  $("#include-body").load("/html/animes/DevilmanCrybaby.html");
+};
+function ks() {
+  $("#include-body").load("/html/animes/KaguyaSama.html");
+};
+function lain() {
+  $("#include-body").load("/html/animes/Lain.html");
+};
+function monster() {
+  $("#include-body").load("/html/animes/Monster.html");
+};
+function opm() {
+  $("#include-body").load("/html/animes/OnePunchMan.html");
+};
+function parasyte() {
+  $("#include-body").load("/html/animes/Parasyte.html");
+};
+function pp() {
+  $("#include-body").load("/html/animes/PsychoPass.html");
+};
+function tg() {
+  $("#include-body").load("/html/animes/TokyoGhoul.html");
+};
