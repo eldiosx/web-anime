8f23e5884cf6c8728584de64b5102f8851d49fb6
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-02-04 15:39:36 +0100

    minorfix-coments

--- js/smart.js
+++ js/smart.js
@@ -28,61 +28,34 @@ function themeToggle() {
   document.getElementById("theme").textContent = localStorage.getanime("theme");
 }
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain attribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-};
 //Smart-animes (experimental)
 function aot() {
-  $("#include-body").load("html/xhtml/AttackOnTitan.xhtml");
+  $("#include-content").load("html/xhtml/AttackOnTitan.xhtml");
 };
 function dn() {
-  $("#include-body").load("html/xhtml/DeathNote.xhtml");
+  $("#include-content").load("html/xhtml/DeathNote.xhtml");
 };
 function dcb() {
-  $("#include-body").load("html/xhtml/DevilmanCrybaby.xhtml");
+  $("#include-content").load("html/xhtml/DevilmanCrybaby.xhtml");
 };
 function ks() {
-  $("#include-body").load("html/xhtml/KaguyaSama.xhtml");
+  $("#include-content").load("html/xhtml/KaguyaSama.xhtml");
 };
 function lain() {
-  $("#include-body").load("html/xhtml/Lain.xhtml");
+  $("#include-content").load("html/xhtml/Lain.xhtml");
 };
 function monster() {
-  $("#include-body").load("html/xhtml/Monster.xhtml");
+  $("#include-content").load("html/xhtml/Monster.xhtml");
 };
 function opm() {
-  $("#include-body").load("html/xhtml/OnePunchMan.xhtml");
+  $("#include-content").load("html/xhtml/OnePunchMan.xhtml");
 };
 function parasyte() {
-  $("#include-body").load("html/xhtml/Parasyte.xhtml");
+  $("#include-content").load("html/xhtml/Parasyte.xhtml");
 };
 function pp() {
-  $("#include-body").load("html/xhtml/PsychoPass.xhtml");
+  $("#include-content").load("html/xhtml/PsychoPass.xhtml");
 };
 function tg() {
-  $("#include-body").load("html/xhtml/TokyoGhoul.xhtml");
+  $("#include-content").load("html/xhtml/TokyoGhoul.xhtml");
 };
