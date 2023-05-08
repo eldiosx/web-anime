bf92ca6f2d0693aa15f415a67bdde3d8724a08cb
Daniel Delgado Jiménez <danieldelgadojimenez@protonmail.com>
2023-05-08 12:25:19 +0200

    sASA

--- js/smart.js
+++ js/smart.js
@@ -8,7 +8,6 @@ const cookieConsent = document.getElementById("cookie-consent");
 const acceptCookiesButton = document.getElementById("accept-cookies");
 const rejectCookiesButton = document.getElementById("reject-cookies");
 
-
 function setCookie(name, value, days) {
   const date = new Date();
   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
